6b9b767e1dfef1b5d7ec0eb1b659da33122bd4f4
Lars Nielsen <larn@tv2.dk>
2016-11-27 20:13:37 +0100

    Added FAB and refresh layout to gallery activity

--- app/src/main/java/dk/lndesign/explicitimage/GalleryActivity.java
+++ app/src/main/java/dk/lndesign/explicitimage/GalleryActivity.java
@@ -4,6 +4,7 @@
 package dk.lndesign.explicitimage;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,7 +20,10 @@ import dk.lndesign.explicitimage.model.ExplicitImage;
  */
 public class GalleryActivity extends AppCompatActivity {
 
-    DatabaseController mDatabase = new DatabaseController();
+    private DatabaseController mDatabase = new DatabaseController();
+
+    private GalleryRecyclerAdapter mRecycleAdapter;
+    private SwipeRefreshLayout mRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +31,34 @@ public class GalleryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gallery);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        final GalleryRecyclerAdapter recyclerAdapter = new GalleryRecyclerAdapter();
-        recyclerView.setAdapter(recyclerAdapter);
+        mRecycleAdapter = new GalleryRecyclerAdapter();
+        recyclerView.setAdapter(mRecycleAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
+        mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateImages();
+            }
+        });
+
+        updateImages();
+    }
+
+    private void updateImages() {
         mDatabase.getExplicitImages(new DatabaseController.LoadingCallback<List<ExplicitImage>>() {
             @Override
             public void onDataChange(List<ExplicitImage> images) {
-                recyclerAdapter.clearItems();
-                recyclerAdapter.updateItems(images);
+                mRecycleAdapter.clearItems();
+                mRecycleAdapter.updateItems(images);
+
+                mRefreshLayout.setRefreshing(false);
             }
 
             @Override
             public void onCancelled() {
-
+                mRefreshLayout.setRefreshing(false);
             }
         });
     }
